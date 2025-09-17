093a9a5d858b2bd03238fb077b82639c5882a03f
Team-ZED-DIGITAL <team@zed.digital>
2025-09-17 17:58:23 +0530

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,23 +1,22 @@
+// swift-tools-version: 5.9
 import PackageDescription
 
-
-
 let package = Package(
-    name: "ZEDPayment",
-    platforms: [
-        .iOS(.v13)
-    ],
-    products: [
-        .library(
-            name: "ZEDPayment",
-            targets: ["ZEDPayment"]
-        ),
-    ],
-    targets: [
-        .binaryTarget(
-            name: "ZEDPayment",
-            url: "https://github.com/Team-ZED-DIGITAL/ZED-Payment/releases/download/1.0.0/ZEDPayment.xcframework.zip",
-            checksum: "4ded8a55f49ad06f30715b53877182a4e9b9394a16a2727de96cc95f4a7e856f"
-        )
-    ]
+    name: "ZEDPayment",
+    platforms: [
+        .iOS(.v13)
+    ],
+    products: [
+        .library(
+            name: "ZEDPayment",
+            targets: ["ZEDPayment"]
+        ),
+    ],
+    targets: [
+        .binaryTarget(
+            name: "ZEDPayment",
+            url: "https://github.com/Team-ZED-DIGITAL/ZED-Payment/releases/download/1.0.0/ZEDPayment.xcframework.zip",
+            checksum: "4ded8a55f49ad06f30715b53877182a4e9b9394a16a2727de96cc95f4a7e856f"
+        )
+    ]
 )
